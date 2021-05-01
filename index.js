fb240dbc6edd05401063cda1e344eb4928e3f5ab
aki <lala.akira@gmail.com>
2021-05-01 12:44:40 +0900

    chore: Add config

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 module.exports = {
-  printWidth: 120,
-  semi: false,
-  singleQuote: true,
-  bracketSpacing: true,
-  trailingComma: 'all',
-  arrowParens: 'always',
-  useTabs: false,
+  plugins: ['stylelint-order', 'stylelint-scss'],
+  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  rules: {
+    // NOTE: scssで必要
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+
+    'order/properties-alphabetical-order': true,
+
+    // NOTE: vendor prefixはautoprefixer任せでOK
+    'at-rule-no-vendor-prefix': true,
+    'property-no-vendor-prefix': true,
+    'media-feature-name-no-vendor-prefix': true,
+    'selector-no-vendor-prefix': true,
+    'value-no-vendor-prefix': true,
+  },
 }
