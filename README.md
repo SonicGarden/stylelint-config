1e7144f8540f7a7edc0e10f9f1810c5d9f5d8175
aki <lala.akira@gmail.com>
2021-05-01 12:41:00 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# stylelint-config
+
+## Usage
+
+Install the package using `npm` (or `yarn`)
+
+```sh
+npm install --save-dev @sonicgarden/stylelint-config
+```
---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  useTabs: false,
+}
